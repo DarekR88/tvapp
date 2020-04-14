a220cd84028ea443fd6a04a17bab84397971c36c
DarekR88 <dradmclad@gmail.com>
2020-04-14 14:21:10 -0500

    redux file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
+    <Provider>
     <div className="App">
-      
+      <SearchBar />
     </div>
+    </Provider>
   );
 }
 
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState('Search for a TV Show')
+    const [searchFocus, setSearchFocus] = useState(false)
+
+    const handleChange = (event) => { 
+        setSearchTerm(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(searchTerm)
+        // setSearchFocus(false)
+    }
+
+    const inputFocus = () => {
+        setSearchTerm('')
+        setSearchFocus(true)
+    }
+
+    let dynamicLabel;
+    
+    if (searchFocus) {
+        dynamicLabel = <p>Search for a TV Show</p>
+    }
+
   return (
     <div className="SearchBar">
-      
+      <form className='searchForm' onSubmit={handleSubmit}>
+          {dynamicLabel}
+        <input onFocus={inputFocus} onChange={handleChange} type="text" id="search-field" name="search-field" value={searchTerm}></input>
+        <input type="submit" value="Search"></input>
+      </form>
     </div>
   );
 }
