e712bb5cfe977e65d90d6cc1106d7e9a04d44cec
DarekR88 <dradmclad@gmail.com>
2020-05-04 10:57:41 -0500

    more data displayed

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 function Card(props) {
+  return (
+    <div className="Card">
+      <p>{props.name}</p>
+      <p>{props.type}</p>
+      <p>{props.genres}</p>
+      <p>{props.dDay}</p>
+      <p>{props.url}</p>
+    </div>
+  );
+}
 
-    return (
-      <div className="Card">
-          <p>{props.name}</p>
-          <p>{props.type}</p>
-          <p>{props.genres}</p>
-      </div>
-    );
-  }
-  
-  export default Card;
+export default Card;
--- src/components/ResultsContainer/index.js
+++ src/components/ResultsContainer/index.js
@@ -1,34 +1,74 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from 'react-redux';
-import Card from '../Card';
+import { useSelector } from "react-redux";
+import Card from "../Card";
 
 function ResultsContainer() {
+  const showSearchResults = useSelector(
+    (state) => state.searchResults.searchResults.showSearchResults
+  );
+  const peopleSearchResults = useSelector(
+    (state) => state.peopleResults.peopleSearchResults.peopleSearchResults
+  );
 
-    const searchResults = useSelector(state => state.searchResults.searchResults.showSearchResults);
-  
-    const resultsFunc = (searchResults) => {
-      if (searchResults){
-        const newArr = searchResults.map( i => {
-          console.log(i.show.name)
-          return( 
-            <Card 
+  const showResultsFunc = (searchResults) => {
+    if (searchResults) {
+      const newArr = searchResults.map((i) => {
+        // console.log(i.show.name)
+        return (
+          <div className="showContainer">
+            <Card
               name={i.show.name}
               type={i.show.type}
-              genres={i.show.genres.join(', ')}
+              genres={i.show.genres.join(", ")}
+              url={i.show.url}
+            />
+          </div>
+        );
+      });
+      return newArr;
+    }
+  };
 
+  const peopleResultsFunc = (searchResults) => {
+    if (searchResults) {
+      const peopleArr = searchResults.map((i) => {
+        if (
+          i.person.country &&
+          i.person.birthday &&
+          i.person.deathday &&
+          i.person.url
+        ) {
+          if (i.person.country.name) {
+            return (
+              <Card
+                name={i.person.name}
+                type={i.person.country.name}
+                genres={i.person.birthday}
+                dDay={i.person.deathday}
+                url={i.person.url}
+              />
+            );
+          }
+        } else {
+          return (
+            <Card 
+            name={i.person.name}
+            type={i.person.birthday}
+            url={i.person.url}
             />
           )
-        })
-        return newArr
-      }
+        }
+      });
+      return peopleArr;
     }
+  };
 
-    return (
-      <div className="ResultsContainer">
-        {resultsFunc(searchResults)}
-      </div>
-    );
-  }
-  
-  export default ResultsContainer;
-  
+  return (
+    <div className="ResultsContainer">
+      {showResultsFunc(showSearchResults)}
+      {peopleResultsFunc(peopleSearchResults)}
+    </div>
+  );
+}
+
+export default ResultsContainer;
