8e98c0344ada76c4cdd1a0dd3a8e42d9504bcbad
DarekR88 <dradmclad@gmail.com>
2020-05-16 13:58:27 -0500

    filter buttons

--- src/components/FilterButtons/index.js
+++ src/components/FilterButtons/index.js
@@ -2,14 +2,24 @@ import React from "react";
 
 function Filter() {
 
-  const handleChange = () => {
+  const handleClick = () => {
     
   }
 
   return (
     <div className="filterContainer">
-
-
+      <p>Search for: </p>
+      <div className="filterButtons">
+        <button class="favorite styled" type="button">
+          Shows
+        </button>
+        <button class="favorite styled" type="button">
+          Actors
+        </button>
+        <button class="favorite styled" type="button">
+          Actors and Shows
+        </button>
+      </div>
     </div>
   );
 }
